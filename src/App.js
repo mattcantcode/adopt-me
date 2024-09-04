8bca4356219d8c98e31033f3045336d43852237e
Matthew Pitman <matt@everybodydancenow.net>
2024-09-04 18:26:35 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      animal: "Dog",
+      breed: "Havanese",
+      name: "Luna",
+    }),
+    React.createElement(Pet, {
+      animal: "Bird",
+      breed: "Cockatiel",
+      name: "Pepper",
+    }),
+    React.createElement(Pet, {
+      animal: "Cat",
+      breed: "Mixed",
+      name: "Doink",
+    }),
+  ]);
+};
+const container = document.getElementById("root");
+const myRoot = ReactDOM.createRoot(container);
+myRoot.render(React.createElement(App));
