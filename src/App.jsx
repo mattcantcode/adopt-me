829e3abf09f5ac6429fc63944fd28ba59ecce404
Matthew Pitman <matt@everybodydancenow.net>
2024-09-18 11:58:48 +0100

    interim checkin for debugging

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,13 @@
-import { createRoot } from "react-dom";
-import Pet from "./Pet";
+import { createRoot } from "react-dom/client";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  <div>
-    <h1>Adopt Me!</h1>
-    <Pet name="Luna" breed="Havanese" animal="Dog" />
-    <Pet name="Pepper" breed="Cockatiel" animal="Bird" />
-    <Pet name="Doink" breed="Mixed" animal="Cat" />
-  </div>;
-  /* return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      animal: "Dog",
-      breed: "Havanese",
-      name: "Luna",
-    }),
-    React.createElement(Pet, {
-      animal: "Bird",
-      breed: "Cockatiel",
-      name: "Pepper",
-    }),
-    React.createElement(Pet, {
-      animal: "Cat",
-      breed: "Mixed",
-      name: "Doink",
-    }),
-  ]); */
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 const container = document.getElementById("root");
 const myRoot = createRoot(container);
