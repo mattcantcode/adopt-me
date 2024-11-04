16f5010fbcfff07d3ee8fedd5e8db279a78941d8
mattcantcode <matt@everybodydancenow.net>
2024-11-04 23:28:15 +0000

    modified export to wrap component in an ErrorBoundary first
    also added doggo spinner

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
+import ErrorBoundary from "./ErrorBoundary";
 import fetchPet from "./fetchPet";
 
 const Details = () => {
@@ -9,7 +10,7 @@ const Details = () => {
   if (results.isLoading) {
     return (
       <div className="loading-pane">
-        <h2 className="loader">🤪</h2>
+        <h2 className="loader">🐶</h2>
       </div>
     );
   }
@@ -30,4 +31,12 @@ const Details = () => {
   );
 };
 
-export default Details;
+function DetailsErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
+
+export default DetailsErrorBoundary;
