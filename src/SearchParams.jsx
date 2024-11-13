daa55435f27588223f8f2b955cd8ec593a696376
mattcantcode <matt@everybodydancenow.net>
2024-11-13 22:11:11 +0000

    implemented global context and indicator for adopted pet

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { useQuery } from "@tanstack/react-query";
+import AdoptedPetContext from "./AdoptedPetContext.jsx";
 import Results from "./Results.jsx";
 import useBreedList from "./useBreedList.js";
 import fetchSearch from "./fetchSearch.js";
@@ -14,6 +15,8 @@ const SearchParams = () => {
   });
   const [animal, setAnimal] = useState("");
   const [breeds] = useBreedList(animal);
+  // eslint-disable-next-line no-unused-vars
+  const [adoptedPet, _] = useContext(AdoptedPetContext);
 
   const results = useQuery(["search", requestParams], fetchSearch);
   const pets = results?.data?.pets ?? [];
@@ -32,6 +35,11 @@ const SearchParams = () => {
           setRequestParams(obj);
         }}
       >
+        {adoptedPet ? (
+          <div className="pet image-container">
+            <img src={adoptedPet.images[0]} alt={adoptedPet.name} />
+          </div>
+        ) : null}
         <label htmlFor="location">
           Location
           <input name="location" id="location" placeholder="Location" />
